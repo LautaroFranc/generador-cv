20d7315f516c9e3d61631e15d8aa7efd10b1e004
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2023-05-27 17:50:54 -0300

    fix diploy

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -2,27 +2,27 @@ import { useState } from "react"
 import axios from "axios"
 
 const useFetchRequest = (URL_API:string, method:"post"|"get") =>{
-  const [response, setResponse] =useState<any>()
+  const [response, setResponse] =useState<{}>()
   const [messageError, setMessageError] =useState()
 
     const request =async (data:object) =>{
     
     try {
-      let bodyContent = JSON.stringify(data);
+      const bodyContent = JSON.stringify(data);
     
-      let headersList = {
+      const headersList = {
         "Accept": "*/*",
         "Content-Type": "application/json" 
        }
        
-      let reqOptions = {
+      const reqOptions = {
         url: URL_API,
         method: method,
         headers: headersList,
         data: bodyContent,
       }
 
-      let result = await axios.request(reqOptions)
+      const result = await axios.request(reqOptions)
       setResponse(result)
 
       } catch (error:any) {
