53db7ccb2af57350b50b3bb83504caf60bb8726f
Marcel <98562281+LautaroFranc@users.noreply.github.com>
2023-05-30 17:02:50 -0300

    Update useFetch.ts

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -2,7 +2,7 @@ import { useState } from "react"
 import axios from "axios"
 
 const useFetchRequest = (URL_API:string, method:"post"|"get") =>{
-  const [response, setResponse] =useState<{}>()
+  const [response, setResponse] =useState<any>()
   const [messageError, setMessageError] =useState()
 
     const request =async (data:object) =>{
